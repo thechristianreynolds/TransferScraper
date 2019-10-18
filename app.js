b13e5071b9c5e25b8cbaed741c07cc3d25b5a8a7
Chris <thechristianreynolds@gmail.com>
2019-10-17 21:57:52 -0500

    log schools and courses in alphabetical order

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ let options = {
     url: 'https://tes.collegesource.com/publicview/TES_publicview01.aspx?rid=1bc75ce2-df3e-4e64-baba-dd0f1eff5628&aid=5a1dd94e-cc36-4f55-81c3-98186e4be965',
     method: 'POST',
     headers: headers,
-    body: url1 + '2' + url2 
+    body: url1 + '2' + url2
 }
 
 const server = http.createServer(function (req, res) {
@@ -35,47 +35,64 @@ server.listen(3000, function () {
     console.log("Server started on port 3000");
 });
 
-rp(options)
-    .then(function(response) {
-        const $ = cheerio.load(response);
-        let schoolNames = $('a.gdv_boundfield_uppercase');
-        let schools = [];
-        for(let i = 0; i < schoolNames.length; i++){
-            schools[i] = {
-                name: schoolNames.eq(i).text(),
-                href: schoolNames.eq(i).attr('href').substring(42, 44),
-                courses: ''
-            }
-        }
-        return schools;
-    })
-    .then(function(response2) {
-        for(let i = 0; i < response2.length; i++){
-            processData(response2, i)
-            .then(function(awaitedcourses) {
-                response2[i].courses = awaitedcourses;
-                console.log(response2[i]);
-            })
-        }
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
-
-    
-async function processData(array, index) {
+async function getCoursesFromSchool(school) {
     try {
-        let courseOptions = {
+        let newschool = school;
+            let courseOptions = {
+                url: 'https://tes.collegesource.com/publicview/TES_publicview01.aspx?rid=1bc75ce2-df3e-4e64-baba-dd0f1eff5628&aid=5a1dd94e-cc36-4f55-81c3-98186e4be965',
+                method: 'POST',
+                headers: headers,
+                body: courseurl1 + school.href + courseurl2
+            }
+            let response = await rp(courseOptions);
+            const $2 = cheerio.load(response);
+            let awaitedcourse = $2('td.gdv_boundfield_uppercase'); //.text().replace(/\s\s+/g, ' ');
+            let courseList = []
+            for (let i = 0; i < awaitedcourse.length; i += 4) {
+                let course = {
+                    awayname: awaitedcourse.eq(i).text().replace(/\s\s+/g, ' '),
+                    homename: awaitedcourse.eq(i + 1).text().replace(/\s\s+/g, ' ')
+                }
+                courseList.push(course);
+            }
+            newschool.courses = courseList;
+            return school;
+    } catch (error) {
+
+    }
+}
+
+async function getSchoolsFromPage(page) {
+    try {
+        let options = {
             url: 'https://tes.collegesource.com/publicview/TES_publicview01.aspx?rid=1bc75ce2-df3e-4e64-baba-dd0f1eff5628&aid=5a1dd94e-cc36-4f55-81c3-98186e4be965',
             method: 'POST',
             headers: headers,
-            body: courseurl1 + array[index].href + courseurl2 
+            body: url1 + page + url2
         }
-        let response = await rp(courseOptions);
-        const $2 = cheerio.load(response);
-        let awaitedcourses = $2('td.gdv_boundfield_uppercase').text().replace(/\s\s+/g, ' ');
-        return awaitedcourses;
+        let response = await rp(options);
+        const $ = cheerio.load(response);
+        let schoolNames = $('a.gdv_boundfield_uppercase');
+        let schools = [];
+        for (let i = 0; i < schoolNames.length; i++) {
+            schools[i] = {
+                name: schoolNames.eq(i).text(),
+                href: schoolNames.eq(i).attr('href').substring(42, 44),
+                courses: null
+            }
+        }
+        return schools;
     } catch (error) {
-        
+
     }
 }
+
+getSchoolsFromPage(2)
+.then(function(schools) {
+    for(let i = 0; i < schools.length; i++){
+        getCoursesFromSchool(schools[i])
+        .then(function(schoolWithCourses) {
+            console.log(schoolWithCourses);
+        })
+    }
+})
